54ff1f5ca192c769aeddf46c46f842ed87b52d3e
Mateusz@MSI <matkac98@gmail.com>
2020-12-16 18:59:10 +0100

    Space_on_board
    Adding a function to check it, by using if and it's return True/False

--- tic_tac.py
+++ tic_tac.py
@@ -5,6 +5,9 @@ def insertLetter(letter, pos): #pos=position
     board[pos]= letter  #if user click '6', then we will get position number 6
     #position is equal to letter
 
+def spaceIsFree(pos): #we hace to use pos(position), becouse the space will be free for particular position
+    return board [pos]==' ' #when the space is free, then it's returning true
+
 def printBoard(board):
     print('   |   |   ')
     print(' '+ board[1] + ' | '+board[2]+' | '+board[3])
@@ -17,4 +20,11 @@ def printBoard(board):
     print('   |   |   ')
     print(' '+board[7]+' | '+board[8]+' | '+board[9])
     print('   |   |   ')
-print(printBoard)
+
+#this function is check is board full or not 
+def isBoardFull(board): #here we use board value, becouse we use it only when the board is full
+    if board.count(' ') > 1: #tutaj wywołujemy funkcję, która sprawdza dokładną ilość czegoś
+        # w naszym przypadku sprawdza ona czy w tablicy są puste przestrzenie
+        return False #to jest przypadek, gdy tablica nie jest pełna
+    else:
+        return True #to jest przypadek, gdy tablica jest pełna
