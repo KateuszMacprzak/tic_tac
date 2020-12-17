1a8981533fb82387c1838475a4b04830a2da9ac4
Mateusz@MSI <matkac98@gmail.com>
2020-12-17 14:43:12 +0100

    Player_Move
    Rozbudowana funkcja gracza, która składa się z kilku poleceń

--- tic_tac.py
+++ tic_tac.py
@@ -1,16 +1,18 @@
 board=[' ' for x in range(10)] 
-#instead of define it again and again we make a loop in this list
+#all the 9 positions are inputs
+#to jest lista, w której to mamy pustą pozycję, ale w samym programie ma tam być X/O, które wprowadzi użytkownik
 
 def insertLetter(letter, pos): #pos=position
-    board[pos]= letter  #if user click '6', then we will get position number 6
-    #position is equal to letter
+    board[pos]= letter  #pos to zmienna dla pozycji, dzięki niej określimy jaki numer chcemy podać
+    #space in this position will be equal to letter
+    #nie potrzebujemy nic więcej, po prostu ta konkretna pozcycja będzie zastąpiona literą
 
 def spaceIsFree(pos): #we hace to use pos(position), becouse the space will be free for particular position
     return board [pos]==' ' #when the space is free, then it's returning true
 
 def printBoard(board):
     print('   |   |   ')
-    print(' '+ board[1] + ' | '+board[2]+' | '+board[3])
+    print(' '+ board[1] + ' | '+board[2]+' | '+board[3])   #tutaj mamy zaznaczone pierwsze 3 elementy z listy board, mamy dla nich puste przestrzenie, które wprowadzamy
     print('   |   |   ')
     print('------------')
     print('   |   |   ')
@@ -39,3 +41,21 @@ def IsWinner(b,l):
     (b[3] == l and b[6] == l and b[9] == l) or 
     (b[1] == l and b[5] == l and b[9] == l) or
     (b[3] == l and b[5] == l and b[7] == l)
+
+def playerMove():
+    run = True 
+    while run:
+        move = input('please select a position to etner the X between 1 to 9: ')
+        try: #poniżej wszystko bedzie działało, gdy użykownik wpisze liczbę, w innym przypadku mamy except 
+            move = int(move) #to jest zmiana typu input move z string na int 
+            if move > 0 and move <10: #to jest zmienna, która skupia się na tym, aby ruch użykownika był  w zakresie 1-9
+                if spaceIsFree(move): # to jest funkcja sprawdzająca wolną przestrzeń, jeżeli taka jest to wtedy wykonują się kolejne instrukcje 
+                    run=False          #to dzieję się tylko wtedy,  jeżeli ta konkretna (1-9) przestrzeń jest wolna 
+                    insertLetter('X', move) #x to znak użykownika, a pos, na którym jest zdefiniowa funkcja move użykownik wprowadza na początku
+                else: #jeżeli pozycja jest zajęta, to wtedy wyskakuje nam komunikat, że ta pozycja jest zajęta. Działa na podstawie funkcji spaceIsFree
+                    print('Sorry, this space is occupied') 
+            else:
+                print("Please type a number between 1 and 9 ") # to jest komunikat, który będzie, gdy użykownik wpisze liczbę, która nie jest w zakresie 
+        except:
+            print('Please type a number')  #to się dzieję, gdy użytkownik wpiszę słowa zamiast liczb
+
